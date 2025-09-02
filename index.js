1207d289c08dac0d78020125bd99862fe7640a6b
grasmarcussanti129 <wemmelkiarie@hotmail.com>
2025-09-02 11:23:59 -0500

    Updated response message for clarity.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Automated CI Pipeline is running!');
+  res.send('The CI Pipeline is up and running!');
 });
 
 app.listen(port, () => {
